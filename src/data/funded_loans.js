a316f9371f69119f57c295bef56969dc2bc96a77
Sanderfer Chau <sanderfer.chau@gmail.com>
2016-11-17 21:58:22 -0800

    Add skeleton files

---
+++ src/data/funded_loans.js
@@ -0,0 +1,21 @@
+const fundedLoans = [
+  {
+    'id': 'p1',
+    'title': 'BRIDGE FINANCING FOR PURCHASE IN POINT RICHMOND, CA',
+    'Loan Amount': '$770,000',
+    'Combined Loan to Value': '55%',
+    'purpose': 'Bridge Financing for Purchase',
+    'loan_position': '1st Deed of Trust on Two Properties',
+    'term': '355 Calendar Days',
+    'property_type': 'Two Residential Condominiums with SF/Bay Views',
+    'images': [
+      { 'img1': 'p1-1.jpg'},
+      { 'img2': 'p1-1.jpg' },
+    ],
+  },
+  {
+    // ...
+  }
+];
+
+export default fundedLoans;
